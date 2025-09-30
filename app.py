5fb357ef103c5b9bfe098eff2f8db7a6a592c566
hmhung2202-hue <hmhung2202@gmail.com>
2025-10-01 00:39:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from flask import Flask, send_from_directory
-import os
 
 app = Flask(__name__)
 
+# Route mặc định để test
 @app.route('/')
 def home():
     return "Flask app running for Zalo OA integration!"
 
-# Route để phục vụ file xác minh Zalo
-@app.route('/<path:filename>')
-def verify_file(filename):
-    return send_from_directory(os.getcwd(), filename)
+# Route phục vụ file xác minh
+@app.route('/<filename>')
+def verify(filename):
+    return send_from_directory('.', filename)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
